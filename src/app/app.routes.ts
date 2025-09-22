9f2fa07510e32bb0256ef4666bd446a6315788b3
cristian <cchura.cpc@gmail.com>
2025-09-22 08:11:31 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './layout/component/app.layout';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {
         path: '',
         component: AppLayout,
+        children: [
+            {
+                path: '',
+                component: Home
+            }
+        ]
 
     }
 ];
